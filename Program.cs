ff0107052701133438a2d339a8a0c8e4c93e9cd5
Ochuwa-sophie <Sophieobomighie@gmail.com>
2020-08-21 17:08:53 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace firstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            Console.Write("Please enter the first number: ");
+            //This line is a comment. It doesn't affect our program.
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            //We are adding the values of firstNumber and 
+            //secondNumber and saving the result in result variable
+            result = firstNumber + secondNumber;
+            Console.WriteLine("The answer is " + result);
+
+        }
+    }
+}
